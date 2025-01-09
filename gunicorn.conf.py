6488a076a17a2c4f9c67386106e69ba6263aa317
piyushgpt <piyushgpt10102004@gmail.com>
2025-01-09 19:54:31 +0530

    changes

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,7 @@
-# gunicorn.conf.py
 workers = 1
 timeout = 300
 worker_class = 'sync'
 bind = "0.0.0.0:10000"
-preload_app = False  
-max_requests = 1    
-max_requests_jitter = 1 
+preload_app = True  # Preload the app to avoid reloading
+max_requests = 10   # Restart workers periodically
+max_requests_jitter = 5  # Add randomness to worker restarts
